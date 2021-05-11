b5c3aa67a0a96ee167f067c3b2b20607547d9583
Yilfri Salave <yilfri97@hotmail.com>
2021-05-11 17:50:12 -0400

    Presupuesto agregado al State

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Pregunta from './components/Pregunta';
 
 function App() {
+	const [presupuesto, setPresupuesto] = useState(0);
+	const [restante, setRestante] = useState(0);
+
 	return (
 		<div className="container">
 			<header>
 				<h1>Gasto Semanal</h1>
 
 				<div className="contenido contenido-principal">
-					<Pregunta />
+					<Pregunta setPresupuesto={setPresupuesto} setRestante={setRestante} />
 				</div>
 			</header>
 		</div>
--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Error from './Error';
 
-const Pregunta = () => {
+const Pregunta = ({ setPresupuesto, setRestante }) => {
 	const [cantidad, setCantidad] = useState(0);
 	const [error, setError] = useState(false);
 
@@ -21,6 +21,8 @@ const Pregunta = () => {
 		}
 
 		setError(false);
+		setPresupuesto(cantidad);
+		setRestante(cantidad);
 	};
 
 	return (
